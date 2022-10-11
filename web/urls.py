701609ecff67ea34111baa34d08f2bd8586ef961
bulatsabirow <mlbulat@mail.ru>
2022-10-11 19:33:47 +0300

    add product page

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from web.views import products_page
+from web.views import products_page, product_view
 
 urlpatterns = [
-    path('main/', products_page, name='products')
+    path('products/', products_page, name='products'),
+    path('product/<int:id>', product_view, name='product'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from web.models import Product
 
@@ -10,5 +10,12 @@ def products_page(request):
     products = Product.objects.all().order_by('-created_at')
     return render(request, 'web/main.html', {
         'products': products,
+    })
+
+
+def product_view(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'web/product.html', {
+        'product': product,
     })
 
